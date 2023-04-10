f5abad8e35884d864f48f333dc0c268c73344fbd
kyeongho <dagh1218@naver.com>
2023-04-10 23:21:40 +0900

    #15 DTO 객체들 toString 추가
    - 테스트 편의를 위해 toString 추가

--- calendar-alarm-manager/src/main/java/com/guardjo/calendar/alarm/manager/domain/GoogleAccountInfo.java
+++ calendar-alarm-manager/src/main/java/com/guardjo/calendar/alarm/manager/domain/GoogleAccountInfo.java
@@ -2,9 +2,11 @@ package com.guardjo.calendar.alarm.manager.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
 @AllArgsConstructor(staticName = "of")
+@ToString
 public class GoogleAccountInfo {
     private String id;
     private String email;
